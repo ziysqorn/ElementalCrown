b06d72ecde9ea4d8e191ebc8d53066e7ebf4bcad
ziysqorn <tranduyquan2003@gmail.com>
2024-06-20 22:27:47 +0700

    Added SwordSkeleton

---
+++ Source/ElementalCrown/Characters/Enemy/Enemy_SwordSkeleton.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy_SwordSkeleton.h"
+
+AEnemy_SwordSkeleton::AEnemy_SwordSkeleton()
+{
+	//Set character's base stats
+	MaxHealth = Default_Character_MaxHealth - 50;
+	CurrentHealth = MaxHealth;
+	GetCharacterMovement()->MaxWalkSpeed = 110.0f;
+	//Set character's element
+	ElementalType = new Metal();
+	//
+	PlayerDetectBox = FVector(150, 0, 40);
+	WallDetectBox = FVector(20, 0, 40);
+}
---
+++ Source/ElementalCrown/Characters/Enemy/Enemy_SwordSkeleton.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseEnemyCharacter.h"
+#include "Enemy_SwordSkeleton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELEMENTALCROWN_API AEnemy_SwordSkeleton : public ABaseEnemyCharacter
+{
+	GENERATED_BODY()
+
+public:
+	//************************************* CONSTRUCTOR *****************************************
+	AEnemy_SwordSkeleton();
+	//************************************* EVENTS **********************************************************
+	//************************************* ACTIONS *********************************************************
+};
